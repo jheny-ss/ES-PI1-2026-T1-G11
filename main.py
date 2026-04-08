b30005266cff350f052ade459a892eba7e89d53d
ArthurDeSenna <arthur.snc@puccampinas.edu.br>
2026-04-08 11:44:37 -0300

    feat: create menu p1

---
+++ main.py
@@ -0,0 +1,49 @@
+print("-"*50)
+print("MENU".center(50))
+print("-"*50)
+print("\n1 - GERENCIAMENTO")
+print("2 - VOTAÇÃO")
+print("3 - RESULTADOS")
+print("4 - AUDITORIA")
+opc = int(input("\nSELECIONE UMA OPÇÃ0: "))
+print("-"*50)
+
+match opc:
+    case 1:
+        print("-"*50)
+        print("")
+        print("GERENCIAMENTO".center(50, "-"))
+        print("\n1 - ELEITORES")
+        print("2 - CANDIDATOS")
+        opc1 = int(input("\n QUEM VOCÊ DESEJA GERENCIAR: "))
+        print("-"*50)
+
+        match opc1:
+            case 1:
+                print("")
+                print("\nELEITORES".center(50, "-"))
+            case 2:
+                print("")
+                print("CANDIDATOS".center(50, "-"))
+
+
+    case 2:
+        print("-"*50)
+        print("")
+        print("VOTAÇÃO".center(50, "-"))
+        print("\n1 - ABRIR SISTEMA")
+        print("2 - VOTAR")
+        print("3 - ENCERRAR VOTAÇÃO")
+        opc2 = int(input("\n O QUE VOCÊ DESEJA FAZER: "))
+        print("-"*50)
+
+        match opc2:
+            case 1:
+                print("")
+                print("\nABRIR SISTEMA".center(50, "-"))
+            case 2:
+                print("")
+                print("VOTAR".center(50, "-"))
+            case 3:
+                print("")
+                print("ENCERRAR VOTAÇÃO".center(50, "-"))
