cec49a3bf3dfa0aa37f1d510b94b936238a41608
duzinetti <eduardo.zb@puccampinas.edu.br>
2026-04-24 11:06:52 -0300

    feat: create folder views and menus functions (print_line, show_menu, main_menu, management_menu)

---
+++ views/menus.py
@@ -0,0 +1,51 @@
+def print_line():
+    """
+    Exibe uma linha horizontal no terminal para melhorar a organização visual.
+    """
+    print("-" * 50)
+
+def show_menu(title, options):
+    """
+    Exibe um menu dinâmico com base em um título e uma lista de opções.
+
+    Args:
+        title (str): Título do menu.
+        options (list): Lista de opções que serão exibidas.
+
+    Returns:
+        int: Opção selecionada pelo usuário.
+        None: Caso a entrada seja inválida.
+    """
+    print_line()
+    print(title.center(50))
+    print_line()
+
+    for i, option in enumerate(options, start=1):
+        print(f"{i} - {option}")
+
+    try:
+        choice = int(input("\nSELECIONE UMA OPÇÃO: "))
+        print_line()
+        return choice
+    except ValueError:
+        print("\n Entrada inválida! Tente novamente.")
+
+
+def main_menu():
+    """
+    Exibe o menu principal do sistema.
+
+    Returns:
+        int: Opção escolhida pelo usuário no menu principal.
+    """
+    return show_menu("MENU", ["GERENCIAMENTO", "VOTAÇÃO", "RESULTADOS", "AUDITORIA", "ENCERRAR"])
+
+
+def management_menu():
+    """
+    Exibe o menu de gerenciamento do sistema.
+
+    Returns:
+        int: Opção escolhida (Eleitores ou Candidatos).
+    """
+    return show_menu("GERENCIAMENTO", ["ELEITORES", "CANDIDATOS", "VOLTAR"])
