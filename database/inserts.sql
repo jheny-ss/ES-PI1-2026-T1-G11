526bc7d11e5c45b900f2541fe135aa6a52f6dc72
jheny-ss <jhenyfer.ss@puccampinas.edu.br>
2026-05-26 21:37:20 -0300

    style: adding inserts

--- database/inserts.sql
+++ database/inserts.sql
@@ -1,28 +1,77 @@
-/**
- ============================================================
- INSERTS: ELEITORES
- ============================================================
-*/
-
-INSERT INTO eleitores (cpf, nome, titulo_eleitor, chave_acesso, status_votacao, status_mesario)
+INSERT INTO eleitores
+(nome, titulo_eleitor, cpf, chave_acesso, status_votacao, status_mesario)
 VALUES
-('12345678901', 'Arthur Senna', '1111111111', 'AS1234', FALSE, TRUE),
-('98765432100', 'Maria Silva', '2222222222', 'MS5678', FALSE, FALSE),
-('45678912300', 'João Oliveira', '3333333333', 'JO9012', FALSE, FALSE),
-('32165498700', 'Ana Souza', '4444444444', 'AS3456', FALSE, FALSE),
-('15975348620', 'Carlos Pereira', '5555555555', 'CP7890', FALSE, FALSE);
 
-/**
- ============================================================
- INSERTS: CANDIDATOS
- ============================================================
-*/
+(
+    'Carlos José',
+    'ZSVMOAIZHYOC', -- 674724100108
+    'L6N7PBTK1UN5',
+    'EGKLYOL2', -- CAJ2824
+    FALSE,
+    TRUE
+),
 
-INSERT INTO candidatos (nome, numero_de_votacao, partido)
+(
+    'Jhenyfer Souza',
+    'WOXQN83ZHYOC', -- 484923790108
+    'WO3YPDWK1UDQ',
+    'ZF1AO8VH', -- JHS1409
+    FALSE,
+    TRUE
+),
+
+(
+    'Maria Aparecida',
+    'RGTGRFK4HYOC', -- 276135040108
+    'YOQDUHJ11UN5',
+    'YA50QBP8', -- MAA5506
+    FALSE,
+    FALSE
+),
+
+(
+    'Leticia Dias',
+    'ZS1U2XHYHYOC', -- 678578010108
+    'UJQETHVJ2WTE',
+    '0FB9HYHW', -- LED5012
+    FALSE,
+    FALSE
+),
+
+(
+    'Carla Dias',
+    'SF0URFZRHYO9', -- 526835750132
+    'XOUISEJ2ZQN5',
+    'EGA7XORB', -- CAD4657
+    FALSE,
+    FALSE
+),
+
+(
+    'Daiana Souza',
+    'HYL5QBVJHYZR', -- 011350710175
+    '3ZYNK2YNK4L2',
+    'GJ3ESIP8', -- DAS3286
+    FALSE,
+    FALSE
+),
+
+(
+    'Edson Silva',
+    'O840XPQCHYOC', -- 408857420108
+    'SG0SO9K4HYJZ',
+    'LS2CZSVH', -- EDS2679
+    FALSE,
+    FALSE
+);
+-- ============================================================
+-- INSERTS CANDIDATOS
+-- ============================================================
+
+INSERT INTO candidatos
+(nome, numero_de_votacao, partido)
 VALUES
-('Carlos Andrade', 10, 'Partido A'),
-('Fernanda Lima', 20, 'Partido B'),
-('Ricardo Gomes', 30, 'Partido C'),
-('Juliana Rocha', 40, 'Partido D');
-
-
+('Helena Vargas', 20, 'PDS'),
+('Valerius Kroeger', 30, 'ORDEM'),
+('Aléxia Thorne', 60, 'VANGUARDA'),
+('Odorico Neto', 50, 'PPU');
